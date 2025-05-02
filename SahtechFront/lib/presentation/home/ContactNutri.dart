df0026b25f4d782b45c0971b3be7ff9987f86db3
Raouf fer <feraouf91@gmail.com>
2025-05-02 04:26:35 +0100

    fixed the contacts nutri page

--- SahtechFront/lib/presentation/home/ContactNutri.dart
+++ SahtechFront/lib/presentation/home/ContactNutri.dart
@@ -8,6 +8,195 @@ import 'package:sahtech/presentation/profile/UserProfileSettings.dart';
 import 'package:sahtech/presentation/scan/camera_access_screen.dart';
 import 'package:sahtech/core/utils/models/user_model.dart';
 
+// Widget for displaying nutritionist cards in a vertical list
+class VerticalNutritionistCard extends StatelessWidget {
+  final NutritionistModel nutritionist;
+  final VoidCallback onCallTap;
+  final VoidCallback onDetailsTap;
+
+  const VerticalNutritionistCard({
+    Key? key,
+    required this.nutritionist,
+    required this.onCallTap,
+    required this.onDetailsTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(bottom: 16.h),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey.shade200, width: 1),
+        borderRadius: BorderRadius.circular(16.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.25),
+            spreadRadius: 0,
+            blurRadius: 8,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          // Doctor image with margins
+          Padding(
+            padding: EdgeInsets.all(6.w),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12.r),
+              child: Image.network(
+                nutritionist.profileImageUrl,
+                width: 80.w,
+                height: 98.h,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                    width: 80.w,
+                    height: 98.h,
+                    color: Colors.grey[200],
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.grey[400],
+                      size: 40.r,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+
+          // Doctor info
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 8.w,
+                vertical: 8.h,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Rating
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 14.sp,
+                      ),
+                      SizedBox(width: 2.w),
+                      Text(
+                        nutritionist.rating.toString(),
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  // Name
+                  Text(
+                    nutritionist.name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14.sp,
+                      color: Colors.black87,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+
+                  // Specialization
+                  Text(
+                    nutritionist.specialization,
+                    style: TextStyle(
+                      fontSize: 12.sp,
+                      color: Colors.grey[700],
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+
+                  // Location with icon
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        color: Colors.red[400],
+                        size: 12.sp,
+                      ),
+                      SizedBox(width: 2.w),
+                      Expanded(
+                        child: Text(
+                          nutritionist.location,
+                          style: TextStyle(
+                            fontSize: 11.sp,
+                            color: Colors.grey[600],
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 8.h),
+
+                  // Buttons moved to the right side
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      // Call button
+                      InkWell(
+                        onTap: onCallTap,
+                        child: Container(
+                          padding: EdgeInsets.all(5.w),
+                          decoration: BoxDecoration(
+                            color: Color(0x7D9FE870),
+                            borderRadius: BorderRadius.circular(8.r),
+                          ),
+                          child: Icon(
+                            Icons.phone,
+                            color: Colors.black,
+                            size: 14.sp,
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(width: 8.w),
+
+                      // Arrow button
+                      InkWell(
+                        onTap: onDetailsTap,
+                        child: Container(
+                          padding: EdgeInsets.all(5.w),
+                          decoration: BoxDecoration(
+                            color: Color(0x7D9FE870),
+                            borderRadius: BorderRadius.circular(8.r),
+                          ),
+                          child: Icon(
+                            Icons.arrow_forward,
+                            color: Colors.black,
+                            size: 14.sp,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class ContactNutri extends StatefulWidget {
   final UserModel? userData;
 
@@ -245,206 +434,24 @@ class _ContactNutriState extends State<ContactNutri> {
                           ),
                         ),
                       )
-                    : SingleChildScrollView(
+                    : ListView.builder(
                         padding: EdgeInsets.symmetric(
                             horizontal: 16.w, vertical: 8.h),
-                        child: Column(
-                          children: _filteredNutritionists.map((nutritionist) {
-                            return _buildNutritionistListItem(nutritionist);
-                          }).toList(),
-                        ),
+                        itemCount: _filteredNutritionists.length,
+                        itemBuilder: (context, index) {
+                          final nutritionist = _filteredNutritionists[index];
+                          return VerticalNutritionistCard(
+                            nutritionist: nutritionist,
+                            onCallTap: () => _callNutritionist(nutritionist),
+                            onDetailsTap: () =>
+                                _navigateToNutritionistDetails(nutritionist),
+                          );
+                        },
                       ),
           ),
         ],
       ),
       bottomNavigationBar: _buildBottomNavigationBar(),
-    );
-  }
-
-  Widget _buildNutritionistListItem(NutritionistModel nutritionist) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 16.h),
-      padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20.r),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.1),
-            spreadRadius: 0,
-            blurRadius: 10,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // Nutritionist Image
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10.r),
-            child: Image.network(
-              nutritionist.profileImageUrl,
-              width: 60.w,
-              height: 60.h,
-              fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) {
-                return Container(
-                  width: 60.w,
-                  height: 60.h,
-                  color: Colors.grey.shade200,
-                  child: Icon(
-                    Icons.person,
-                    color: Colors.grey.shade400,
-                    size: 30.r,
-                  ),
-                );
-              },
-            ),
-          ),
-          SizedBox(width: 12.w),
-
-          // Nutritionist Info and Buttons
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                // Nutritionist Info
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      // Name and Rating
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              nutritionist.name,
-                              style: TextStyle(
-                                fontSize: 15.sp,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black87,
-                              ),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                                size: 14.sp,
-                              ),
-                              SizedBox(width: 2.w),
-                              Text(
-                                nutritionist.rating.toString(),
-                                style: TextStyle(
-                                  fontSize: 13.sp,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black87,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 2.h),
-
-                      // Specialization
-                      Text(
-                        nutritionist.specialization,
-                        style: TextStyle(
-                          fontSize: 13.sp,
-                          color: Colors.grey.shade600,
-                        ),
-                      ),
-                      SizedBox(height: 2.h),
-
-                      // Location
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.location_on,
-                            color: Colors.red.shade400,
-                            size: 12.sp,
-                          ),
-                          SizedBox(width: 2.w),
-                          Expanded(
-                            child: Text(
-                              nutritionist.location,
-                              style: TextStyle(
-                                fontSize: 11.sp,
-                                color: Colors.grey.shade600,
-                              ),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-
-                // Buttons Container for right alignment
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    SizedBox(height: 20.h), // Push buttons to bottom
-                    Row(
-                      children: [
-                        // Call Button
-                        Container(
-                          margin: EdgeInsets.only(right: 8.w),
-                          width: 36.w,
-                          height: 36.h,
-                          decoration: BoxDecoration(
-                            color: Color(0xAAB3F492),
-                            shape: BoxShape.circle,
-                          ),
-                          child: IconButton(
-                            icon: Icon(
-                              Icons.phone,
-                              color: Colors.black,
-                              size: 18.sp,
-                            ),
-                            onPressed: () => _callNutritionist(nutritionist),
-                            padding: EdgeInsets.zero,
-                          ),
-                        ),
-
-                        // Details Button - Arrow
-                        Container(
-                          width: 36.w,
-                          height: 36.h,
-                          decoration: BoxDecoration(
-                            color: Color(0xAAB3F492),
-                            shape: BoxShape.circle,
-                          ),
-                          child: IconButton(
-                            icon: Icon(
-                              Icons.arrow_forward,
-                              color: Colors.black,
-                              size: 18.sp,
-                            ),
-                            onPressed: () =>
-                                _navigateToNutritionistDetails(nutritionist),
-                            padding: EdgeInsets.zero,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 
@@ -487,9 +494,12 @@ class _ContactNutriState extends State<ContactNutri> {
       String label, bool isSelected) {
     return InkWell(
       onTap: () {
+        // Don't do anything if the tab is already selected
+        if (isSelected) return;
+
         if (index == 0) {
-          // Navigate back to home
-          Navigator.pop(context);
+          // Navigate back to home - use popUntil to avoid stacking
+          Navigator.of(context).popUntil((route) => route.isFirst);
         } else if (index == 4) {
           // Navigate to profile
           _navigateToProfile();
